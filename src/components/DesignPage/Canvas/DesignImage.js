739a0bc9e334747fb0a4c37afb6c6269e96225ff
Hoang Trinh <piavghoang@gmail.com>
2018-04-10 09:24:57 +0700

    Fix upload image onto canvas

--- src/components/DesignPage/Canvas/DesignImage.js
+++ src/components/DesignPage/Canvas/DesignImage.js
@@ -12,8 +12,16 @@ class DesignImage extends Component {
     };
 
     componentDidMount() {
+        this.updateImage();
+    }
+
+    componentDidUpdate() {
+        this.updateImage();
+    }
+
+    updateImage() {
         const image = new window.Image();
-        image.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYTULZCGUVEQJEXt9iB8PU4Kb2FMS9Z6ufR1FnQTdrEl5uBOl52Q';
+        image.src = this.props.image;
         image.onload = () => {
             // setState will redraw layer
             // because "image" property is changed
@@ -24,7 +32,6 @@ class DesignImage extends Component {
     }
 
     render() {
-        console.log(this.state.image);
         return <Image image={this.state.image} draggable={true}/>;
     }
 }

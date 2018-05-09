610da263e8ab68add2915eaf86738c47c8f1d5cf
Hoang Trinh <hoang.trinhj@gmail.com>
2018-05-09 18:53:44 +0700

    Handle image upload to Cloudinary (client side only)

--- src/containers/DesignPage.js
+++ src/containers/DesignPage.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 import {
     Row,
     Col
@@ -23,9 +24,33 @@ class DesignPage extends Component {
     };
 
     handleFileDrop = files => {
-        this.setState({
-            files,
-            image: files[0].preview
+        const uploaders = files.map(file => {
+            // Initial FormData
+            const formData = new FormData();
+            formData.append("file", file);
+            formData.append("tags", `dezignbox`);
+            formData.append("upload_preset", "ltvpxyqw"); // Replace the preset name with your own
+            formData.append("api_key", "285863254151261"); // Replace API key with your own Cloudinary key
+            formData.append("timestamp", (Date.now() / 1000) | 0);
+
+            // Make an AJAX upload request using Axios (replace Cloudinary URL below with your own)
+            return axios.post("https://api.cloudinary.com/v1_1/dezignbox/image/upload", formData, {
+                headers: { "X-Requested-With": "XMLHttpRequest" },
+            }).then(response => {
+                const data = response.data;
+                const fileURL = data.secure_url; // You should store this URL for future references in your app
+                console.log(data);
+                console.log(fileURL);
+            })
+        });
+
+        // Once all the files are uploaded
+        axios.all(uploaders).then(() => {
+            // ... perform after upload is successful operation
+            this.setState({
+                files,
+                image: files[0].preview
+            });
         });
     };
 

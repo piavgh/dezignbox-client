bb4dea7fdf16b778bba36d0fdfa8984aee19a31b
Hoang Trinh <hoang.trinhj@gmail.com>
2018-06-15 16:08:44 +0700

    Update homepage

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,10 +1,54 @@
 import React, {Component} from 'react';
+import {
+  Row,
+  Col
+} from 'reactstrap';
+
+import "../css/HomePage.css";
 
 export default class HomePage extends Component {
 
   render() {
     return (
-      <p>Home</p>
+      <div className="homepage">
+
+        <Row className="banner">
+          <Col xs={12}>
+            <p>It's time to do you</p>
+            <img src="/images/banner.jpg" alt="banner"/>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col xs={12}>
+            <p>Recommendations For You</p>
+          </Col>
+          <Col xs={4}>
+            <img src="/images/box1.jpg" alt="box1"/>
+          </Col>
+          <Col xs={4}>
+            <img src="/images/box2.jpg" alt="box2"/>
+          </Col>
+          <Col xs={4}>
+            <img src="/images/box3.jpg" alt="box3"/>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col xs={12}>
+            <p>Staff Picks</p>
+          </Col>
+          <Col xs={4}>
+            <img src="/images/box4.jpg" alt="box4"/>
+          </Col>
+          <Col xs={4}>
+            <img src="/images/box5.jpg" alt="box5"/>
+          </Col>
+          <Col xs={4}>
+            <img src="/images/box6.jpg" alt="box6"/>
+          </Col>
+        </Row>
+      </div>
     );
   }
 }

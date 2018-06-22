2ad63587d2cf19ff88162d4eed59819efab7f1c4
Hoang Trinh <hoang.trinhj@gmail.com>
2018-06-22 16:36:07 +0700

    Refactor class name

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Provider} from 'react-redux';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import App from './App';
 
 const Root = ({store}) => (
   <Provider store={store}>
-    <Router>
+    <BrowserRouter>
       <Route path="/" component={App}/>
-    </Router>
+    </BrowserRouter>
   </Provider>
 );
 

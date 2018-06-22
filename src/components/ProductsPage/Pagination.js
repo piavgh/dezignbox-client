470d9d129bf5187ae06fc1d7394eb050812bdbec
Hoang Trinh <hoang.trinhj@gmail.com>
2018-06-22 17:06:49 +0700

    Update pagination component: Add First and Last buttons

--- src/components/ProductsPage/Pagination.js
+++ src/components/ProductsPage/Pagination.js
@@ -18,6 +18,12 @@ export default class AppPagination extends Component {
     this.props.handlePageChange(pageNumber);
   };
 
+  handleFirstPage = () => {
+    if (this.props.currentPage > 1) {
+      this.props.handlePageChange(1);
+    }
+  };
+
   handlePrevPage = () => {
     if (this.props.currentPage > 1) {
       this.props.handlePageChange(this.props.currentPage - 1);
@@ -27,6 +33,12 @@ export default class AppPagination extends Component {
   handleNextPage = () => {
     if (this.props.currentPage < this.props.pageCount) {
       this.props.handlePageChange(this.props.currentPage + 1);
+    }
+  };
+
+  handleLastPage = () => {
+    if (this.props.currentPage < this.props.pageCount) {
+      this.props.handlePageChange(this.props.pageCount);
     }
   };
 
@@ -47,13 +59,29 @@ export default class AppPagination extends Component {
     return (
       <Pagination>
         <PaginationItem disabled={this.props.currentPage <= 1}>
-          <PaginationLink previous onClick={this.handlePrevPage}/>
+          <PaginationLink onClick={this.handleFirstPage}>
+            First
+          </PaginationLink>
+        </PaginationItem>
+
+        <PaginationItem disabled={this.props.currentPage <= 1}>
+          <PaginationLink onClick={this.handlePrevPage}>
+            Previous
+          </PaginationLink>
         </PaginationItem>
 
         {items}
 
         <PaginationItem disabled={this.props.currentPage >= this.props.pageCount}>
-          <PaginationLink next onClick={this.handleNextPage}/>
+          <PaginationLink onClick={this.handleNextPage}>
+            Next
+          </PaginationLink>
+        </PaginationItem>
+
+        <PaginationItem disabled={this.props.currentPage >= this.props.pageCount}>
+          <PaginationLink onClick={this.handleLastPage}>
+            Last
+          </PaginationLink>
         </PaginationItem>
       </Pagination>
     );

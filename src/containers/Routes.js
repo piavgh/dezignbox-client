567b1b303b372d1b4136b19ac674b0fef48a629e
Hoang Trinh <hoang.trinhj@gmail.com>
2018-06-29 10:35:26 +0700

    Refactor Routes

--- src/containers/Routes.js
+++ src/containers/Routes.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import {Route, Switch} from "react-router-dom";
 
 import AppliedRoute from "./AppliedRoute";
@@ -15,17 +15,23 @@ import CheckoutPage from "./CheckoutPage";
 import TransactionsPage from "./TransactionsPage";
 import NotFound from "../components/NotFound";
 
-export default ({childProps}) =>
-  <Switch>
-    <AppliedRoute path="/" exact component={HomePage} props={childProps}/>
-    <UnauthenticatedRoute path="/login" exact component={LoginPage} props={childProps}/>
-    <UnauthenticatedRoute path="/register" exact component={RegisterPage} props={childProps}/>
-    <UnauthenticatedRoute path="/aboutUs" exact component={AboutUs} props={childProps}/>
-    <AuthenticatedRoute path="/start-design" component={DesignPage} props={childProps}/>
-    <AuthenticatedRoute path="/products" exact component={ProductsPage} props={childProps}/>
-    <AuthenticatedRoute path="/products/:id" component={DetailPage} props={childProps}/>
-    <AuthenticatedRoute path="/checkout/:id" component={CheckoutPage} props={childProps}/>
-    <AuthenticatedRoute path="/transactions" exact component={TransactionsPage} props={childProps}/>
-    {/* Finally, catch all unmatched routes */}
-    <Route component={NotFound}/>
-  </Switch>
+export default class Routes extends Component {
+
+    render() {
+        return (
+          <Switch>
+            <AppliedRoute path="/" exact component={HomePage} props={this.props.childProps}/>
+            <UnauthenticatedRoute path="/login" exact component={LoginPage} props={this.props.childProps}/>
+            <UnauthenticatedRoute path="/register" exact component={RegisterPage} props={this.props.childProps}/>
+            <UnauthenticatedRoute path="/aboutUs" exact component={AboutUs} props={this.props.childProps}/>
+            <AuthenticatedRoute path="/start-design" component={DesignPage} props={this.props.childProps}/>
+            <AuthenticatedRoute path="/products" exact component={ProductsPage} props={this.props.childProps}/>
+            <AuthenticatedRoute path="/products/:id" component={DetailPage} props={this.props.childProps}/>
+            <AuthenticatedRoute path="/checkout/:id" component={CheckoutPage} props={this.props.childProps}/>
+            <AuthenticatedRoute path="/transactions" exact component={TransactionsPage} props={this.props.childProps}/>
+            {/* Finally, catch all unmatched routes */}
+            <Route component={NotFound}/>
+          </Switch>
+        )
+    }
+};

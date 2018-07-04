56758fd8855c1b4f5c41923dbaa87926b34838f9
Hoang Trinh <hoang.trinhj@gmail.com>
2018-07-04 11:01:28 +0700

    Update Footer and remove redundant files

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -3,6 +3,7 @@ import {
   Row,
   Col
 } from 'reactstrap';
+import {Link} from 'react-router-dom';
 
 export default class Footer extends Component {
 
@@ -10,24 +11,39 @@ export default class Footer extends Component {
     return (
       <div className="footer">
         <div className="container">
-          <Row>
-            <Col xs={3}>
-              <span><span><i className="far fa-copyright"/></span> 2018 Dezignbox, Inc.</span>
+          <Row className="footer-wrapper">
+            <Col xs={6} className="footer__left-column">
+              <div className="copyright">
+                Copyright <span><i className="far fa-copyright"/></span> 2018 Dezignbox. Powered by Dezignbox.io
+              </div>
+              <div className="link-static-page"><Link to="/">Quy chế hoạt động</Link></div>
+              <div className="link-static-page"><Link to="/">Chính sách bảo mật</Link></div>
+              <div className="link-static-page"><Link to="/">Quy định thanh toán</Link></div>
+              <div className="link-static-page"><Link to="/">Chính sách bảo hành</Link></div>
+              <div className="link-static-page"><Link to="/">Quy trình đặt mua hàng</Link></div>
+              <div className="link-static-page"><Link to="/">Chính sách hoạt động</Link></div>
+              <div className="bo-cong-thuong">
+                <img src="/images/dadangky.png" alt="Da dang ky Bo Cong Thuong"/>
+              </div>
             </Col>
-            <Col xs={3}>
+            <Col xs={6} className="footer__right-column">
+              <div className="social-network-container">
+                <span className="social-network-icon">
+                  <a href="https://www.facebook.com/Dezignbox-179803586196059"
+                     target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-f"/></a>
+                  </span>
+                <span className="social-network-icon"><i className="fab fa-twitter"/></span>
+                <span className="social-network-icon"><i className="fab fa-instagram"/></span>
+                <span className="social-network-icon"><i className="fab fa-pinterest"/></span>
+              </div>
 
-            </Col>
-            <Col xs={3}>
-
-            </Col>
-            <Col xs={3} className="social-network-container">
-              <a href="https://www.facebook.com/Dezignbox-179803586196059" target="_blank" rel="noopener noreferrer">
-                <span className="social-network-icon"><i className="fab fa-facebook-f"/>
-                </span>
-              </a>
-              <span className="social-network-icon"><i className="fab fa-twitter"/></span>
-              <span className="social-network-icon"><i className="fab fa-instagram"/></span>
-              <span className="social-network-icon"><i className="fab fa-pinterest"/></span>
+              <div className="company-information">
+                <p>CÔNG TY TNHH GREEN TECHNOLOGY</p>
+                <p>Mã số thuế: 0201747881</p>
+                <p>Địa chỉ: Số 265 đường Chợ Hàng, Phường Dư Hàng Kênh, Quận Lê Chân, Thành Phố Hải Phòng, Việt Nam</p>
+                <p>Website: dezignbox.io - facebook.com/dezignbox</p>
+                <p>Email: info@dezignbox.io - dezignbox.io@gmail.com</p>
+              </div>
             </Col>
           </Row>
         </div>

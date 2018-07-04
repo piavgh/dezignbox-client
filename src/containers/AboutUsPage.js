3fa4cb07ea0306ab2ec9c14bfac5b5538c1d9827
Hoang Trinh <hoang.trinhj@gmail.com>
2018-07-04 11:04:40 +0700

    Backup AboutUsPage

---
+++ src/containers/AboutUsPage.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+import '../css/AboutUs.css';
+
+export default class AboutUs extends Component {
+
+  render() {
+    return (
+      <div className="about-us-page">
+
+      </div>
+    );
+  }
+}
